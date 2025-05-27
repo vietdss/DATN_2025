be9f270b6598581af72475851b10d17c3a02cacd
vietdss <nguyenhoangviet251103@gmail.com>
2025-05-27 22:29:53 +0700

    update-27/5/2025_v3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,13 +15,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
     <script src="https://cdn.tailwindcss.com"></script>
+<script src="{{ asset('build/assets/app-4ed993c7.js') }}"></script>
 
+   <script src="{{ asset('build/assets/app-eb687449.js') }}"></script>
+<link rel="stylesheet" href="{{ asset('build/assets/app-49001d3f.css') }}">
 
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-    @vite('resources/css/app.css')
+    <!-- @vite('resources/css/app.css') -->
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- @vite(['resources/sass/app.scss', 'resources/js/app.js']) -->
 </head>
 
 <body class="bg-gray-50 min-h-screen flex flex-col" style="overflow: auto;">
